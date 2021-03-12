4422eeba79711f1f6d1d98e54570634b53fbd799
oimauCA <oimauCA@users.noreply.github.com>
2021-03-12 17:24:11 +0100

    Added pdf of lecture and find_zero.py to find roots of a given function.

---
+++ Lezione1/find_zero.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def f(x):
+    return np.sqrt(x) - x - np.log(x + np.random.rand())
+
+tolerance = 1e-5
+interval = [0, 2.5]
+counter = 1
+if f(interval[0]) * f(interval[1]) < 0:
+    diff = 1
+    while np.abs(diff) > tolerance:
+        counter += 1
+        m = np.mean(interval)
+        diff = f(m)
+        if f(interval[0]) * diff < 0:
+            interval = [interval[0], m]
+        elif diff * f(interval[1]) < 0:
+            interval = [m, interval[1]]
+    print(m, diff, counter)
+else:
+    print("Method not appliable or no zeros in interval")
+
+fig, ax = plt.subplots()
+
+ax.plot(np.linspace(1e-3, 2, 1000), f(np.linspace(1e-3, 2, 1000)))
+ax.hlines(0, 0, 2, linewidth = 0.5)
+ax.set_xlabel("x")
+ax.set_ylabel("f(x)")
+ax.set_xlim(0, 2)
+plt.show()
